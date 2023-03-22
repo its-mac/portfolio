94e5ad2cff63cd9d3c2d4f619ae5dca7f5258be4
MAC <bc220415372mab@vu.edu.pk>
2023-03-22 23:40:14 +0500

    Project Uploaded

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Developer",
+    "Graphic Designer",
+    "Web Designer",
+    "Youtuber",
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+  loopCount: true,
+});
